45a03bfddbf20a38da9bb353a1503264c86487cf
MaksymKopych <maximkopych@gmail.com>
2022-04-10 14:02:53 +0200

    animation

---
+++ app.js
@@ -0,0 +1,17 @@
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
+// const glow = document.querySelectorAll(".glow");
+
+setTimeout(() => {
+  header.classList.add("glow");
+}, 500);
+setTimeout(() => {
+  header.style.opacity = 1;
+  main.classList.add("glow");
+  footer.classList.add("glow");
+}, 1500);
+setTimeout(() => {
+  main.style.opacity = 1;
+  footer.style.opacity = 1;
+}, 3000);
